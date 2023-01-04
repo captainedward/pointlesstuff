605561d0d9a6ea5fd0acf16f30461e839795ae75
Edward <95918254+captainedward@users.noreply.github.com>
2023-01-05 00:08:33 +0100

    Update christmas.py

--- christmas.py
+++ christmas.py
@@ -1,4 +1,4 @@
 import random
-#Greatings are written in Italian
+#Greatings are written in Italian, feel free to put your own.
 auguri = ["Anche a te e Famiglia", "Buon Natale e Felice Anno Nuovo!", "Grazie, anche a te!!", "Grazie, Auguri!"]
 print(random.choice(auguri))
