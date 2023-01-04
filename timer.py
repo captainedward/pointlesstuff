ccad8006dfc6812fdca5e307026f5ac1a2870744
Edward <95918254+captainedward@users.noreply.github.com>
2023-01-05 00:04:40 +0100

    translated

--- timer.py
+++ timer.py
@@ -16,7 +16,7 @@ art = """\
 """
 
 def countdown():
-	print("\nIl tuo tea sta infondendo...\n")
+	print("\nYour leaves are infusing...\n")
 
 	total_seconds = tea_time
 	while total_seconds > 0:
@@ -24,10 +24,10 @@ def countdown():
 		print(timer, end="\r")
 		time.sleep(1)
 		total_seconds -= 1
-	print(art, "\nÈ pronto")
+	print(art, "\nYour Tea is ready!")
 #remove this line if you don't want it to make any sound
 	print('\007')	
-tea = int(input("\nVuoi fare un Tea:\n1-Nero\n2-Verde\n3-Custom\n\n"))
+tea = int(input("\nYou want to drink a:\n1-Black Tea\n2-Green Tea\n3-Custom\n\n"))
 while tea != 1 or 2 or 3:
 	if tea == 1:
 		tea_time = 4 * 60
@@ -38,10 +38,10 @@ while tea != 1 or 2 or 3:
 		countdown()
 		break
 	elif tea == 3:
-		m = int(input("\nDimmi quanti minuti vuoi aspettare: "))
-		s = int(input("\nDimmi quanti secondi vuoi aspettare: "))
+		m = int(input("\nHow many minutes: "))
+		s = int(input("\nHow many seconds: "))
 		tea_time = m * 60 + s
 		countdown()
 		break
 	else:python3 make simple beep sound linux
-		tea = int(input("\n" + "Vuoi fare un Tea:\n1-Nero\n2-Verde\n3-Custom\n\n"))
+		tea = int(input("\nYou want to drink a:\n1-Black Tea\n2-Green Tea\n3-Custom\n\n"))
